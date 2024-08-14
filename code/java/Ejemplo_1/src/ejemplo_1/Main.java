ce5c94eb1890db9400c35f90aba14ddf79f329f0
Kralos <c_rojasx@hotmail.com>
2024-08-13 19:06:02 -0600

    ejemplo_1 Java

---
+++ code/java/Ejemplo_1/src/ejemplo_1/Main.java
@@ -0,0 +1,20 @@
+package ejemplo_1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Creamos los objetos
+        Comercial c1 = new Comercial(300, "DDR", 37, 1000);
+        Repartidor r1 = new Repartidor("zona 3", "Fer", 26, 900);
+
+        //Llamamos a plus
+        c1.plus();
+        r1.plus();
+
+        //Mostramos la informacion
+        System.out.println(c1);
+        System.out.println(r1);
+    }
+
+}
